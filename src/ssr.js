f47e681588b9c92c38f3f1362ba2e646b52fef5c
fabioricali <ricalifabio@gmail.com>
2018-08-03 17:02:28 +0200

    experiment

--- src/ssr.js
+++ src/ssr.js
@@ -1,22 +1,49 @@
 const Koa = require('koa');
 const app = new Koa();
 const Doz = require('doz');
+const fs = require('fs');
 
 require('jsdom-global')();
 
-app.use(async ctx => {
-    document.body.innerHTML = `<div id="app"></div>`;
+const rawDoz = fs.readFileSync('./node_modules/doz/dist/doz.min.js');
 
+const js = `
     new Doz({
         root: '#app',
         template() {
-            return `
-                <div>Ciao Mondo</div>
-            `;
+            return \`
+                <div>
+                    <h1>Test</h1>
+                    <button onclick="console.log('ciao')">Click</button>
+                </div>
+            \`;
         }
     });
+`;
 
-    ctx.body = document.body.outerHTML;
+app.use(async ctx => {
+    document.body.innerHTML = `
+        <div id="app"></div>
+    `;
+
+    eval(js);
+
+    const appRender = document.body.innerHTML;
+
+    ctx.body = `<!DOCTYPE html>
+    <html>
+        <head>
+            <title>SSR</title>
+        </head>
+        <body>
+            ${appRender}
+            <script>
+            ${rawDoz}
+            ${js}
+            </script>
+        </body>
+    </html>
+    `;
 });
 
 app.listen(3000);
