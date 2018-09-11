e4687377770d8f09866cc508778b7b902d587c78
fabioricali <ricalifabio@gmail.com>
2018-09-11 12:54:27 +0200

    experiment

--- test/prerender/prerender.js
+++ test/prerender/prerender.js
@@ -1,6 +1,6 @@
 const DozSSR = require('../../index');
 
-const dozSSR = new DozSSR('../../SSR/public/index.html');
+const dozSSR = new DozSSR('../../test/SSR/public/index.html');
 
 (async ()=>{
     await dozSSR.prerender('');
